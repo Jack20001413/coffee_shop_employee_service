c96810fcedfceaa97f1a4f457c6f23cd9a666a7b
Tran Vi Van <van.tranvi@evolutionwellnesssolutionsg.onmicrosoft.com>
2025-01-12 15:01:36 +0000

    feat(workspace): Update workspace's setup

--- .devcontainer/scripts/post-setup.sh
+++ .devcontainer/scripts/post-setup.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
+set -ex;
 
 pip install -r /workspaces/coffee_shop_employee_service/requirements.txt
+
+# Enable project to use .env
+echo "export USE_DOT_ENV=True" >> $HOME/.bashrc && source $HOME/.bashrc
